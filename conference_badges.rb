0ed2cbfcca16f841ce8e26341762c6a40b444a51
A Belgrave <abelgrave95@gmail.com>
2019-02-25 16:08:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(ary)
+  badge_batch = []
+  ary.each { |name| badge_batch << badge_maker(name) }
+  badge_batch
+end
+
+def assign_rooms(speakers)
+  speakers.map_with_index { |name, i|  } { |name| "Hello #{name}! You'll be assigned to room #{i}"  }
+end
