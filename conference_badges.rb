2af1d94504ef4ea9ddea09a4194c830d55176de7
A Belgrave <abelgrave95@gmail.com>
2019-02-25 16:23:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ def batch_badge_creator(ary)
 end
 
 def assign_rooms(speakers)
-  speakers.map.with_index { |name, i| "Hello #{name}! You'll be assigned to room #{i +1}"  }
+  speakers.map.with_index { |name, i| "Hello, #{name}! You'll be assigned to room #{i +1}"  }
 end
 
 def printer(speakers)
